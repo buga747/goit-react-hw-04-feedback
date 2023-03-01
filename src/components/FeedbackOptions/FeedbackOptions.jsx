c752845c241ed86ef64eee08dfc7e7b5c225840d
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-03-01 19:18:27 +0200

    changes package.json

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,25 +1,30 @@
 import PropTypes from 'prop-types';
 import { Container, Button } from './FeedbackOptions.styled';
 
-const capitalize = stringItem => `${stringItem[0].toUpperCase()}${stringItem.slice(1)}`;
+const capitalize = stringItem =>
+  `${stringItem[0].toUpperCase()}${stringItem.slice(1)}`;
 
- function FeedbackOptions({options, onLeaveFeedback }) {
-    return (
-       <Container>
-            {options.map(option =>
-                <Button type='button'
-                    key={option}
-                    onClick={() => { onLeaveFeedback(option) }}>
-                    {capitalize(option)}
-                    </Button>)}
-        </Container> 
-    )
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <Container>
+      {options.map(option => (
+        <Button
+          type="button"
+          key={option}
+          onClick={() => {
+            onLeaveFeedback(option);
+          }}
+        >
+          {capitalize(option)}
+        </Button>
+      ))}
+    </Container>
+  );
 }
-
 
 FeedbackOptions.propTypes = {
-   options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
   onLeaveFeedback: PropTypes.elementType,
-}
+};
 
 export default FeedbackOptions;
